46e991247e2bbea7aba29be2c5b3a524f6855b18
Connor McGaughey <connormcgaughey@Connors-MacBook-Pro.local>
2025-01-14 12:50:01 +0000

    Second commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer() {
+
+    return(
+        <>
+            <footer>
+                <p>My portfolio website</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header() {
+
+    return(
+        <header>
+            <h2>Connor McGaughey</h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">My Work</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </header>
+    )
+}
+export default Header
