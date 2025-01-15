5c4ae11e439447fb45f54dc4a476818d428f7031
Connor McGaughey <connormcgaughey@Connors-MacBook-Pro.local>
2025-01-15 12:16:14 +0000

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 function App() {
 
 
   return (
     <>
       <Header />
-      <Footer />
+
     </>
   );
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import styles from './Header.module.css';
+
+function Header() {
+
+    return(
+        <header>
+            <h2>Connor McGaughey</h2>
+            <ul className={styles.navbar}>
+                <li className={styles.navbutton}><a href="#">Home</a></li>
+                <li className={styles.navbutton}><a href="#">About Me</a></li>
+                <li className={styles.navbutton}><a href="#">My Work</a></li>
+                <li className={styles.navbutton}><a href="#">Contact Me</a></li>
+            </ul>
+        </header>
+    )
+}
+export default Header
