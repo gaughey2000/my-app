af67815eb839faf20d4a104f8eb8c20fccd82562
Connor McGaughey <connormcgaughey@Connors-MacBook-Pro.local>
2025-01-29 12:08:13 +0000

    8th commit

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,15 +1,15 @@
 import styles from './Header.module.css';
-
+import { NavLink } from 'react-router';
 function Header() {
 
     return(
         <header className={styles.header}>
             <h2>Connor McGaughey</h2>
             <ul className={styles.navbar}>
-                <li className={styles.navbutton}><a href="/">Home</a></li>
-                <li className={styles.navbutton}><a href="/aboutme">About Me</a></li>
-                <li className={styles.navbutton}><a href="/mywork">My Work</a></li>
-                <li className={styles.navbutton}><a href="contactme">Contact Me</a></li>
+                <li className={styles.navbutton}><NavLink to="/">Home</NavLink></li>
+                <li className={styles.navbutton}><NavLink to="/aboutme">About Me</NavLink></li>
+                <li className={styles.navbutton}><NavLink to="/mywork">My Work</NavLink></li>
+                <li className={styles.navbutton}><NavLink to="contactme">Contact Me</NavLink></li>
             </ul>
         </header>
     )
